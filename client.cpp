58b90816a3830ec74d79ebdff3c9853070caa59d
KawakamiJustin <kawakami.j@northeastern.edu>
2025-02-21 17:44:50 -0800

    simple Username/Password update

--- client.cpp
+++ client.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout<<"hello world\n";
+    cout << "Hello Welcome to the Stock Trading Platform\nPlease Enter Your User Name and Password\n";
+    string username = "";
+    string password = "";
+    cout << "Username:\t";
+    cin >> username;
+    while(username.length() < 5 || username.length() > 50)
+    {
+        cout << "Please enter a valid Username\n";
+        cout << "Username:\t";
+        cin >> username;
+    }
+    cout << "Password:\t";
+    cin >> password;
+    while(password.length() < 5 || password.length() > 50)
+    {
+        cout << "Please enter a valid Password\n";
+        cout << "Password:\t";
+        cin >> password;
+    }
+    bool validity = false;
+    if(validity)
+    {
+        cout << "Available Commands:\n";
+    }
     return 0;
 }
